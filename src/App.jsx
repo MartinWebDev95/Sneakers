5971ec28dd3fe0db01e15643db1fc210ffa3b4ae
Juan Antonio <jamll.muro@gmail.com>
2023-06-28 09:24:33 +0200

    Add all images from sneakerData.js to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
-import Header from './Header';
+import { useEffect, useState } from 'react';
+import Header from './components/Header';
+import styles from './App.module.css';
+import sneaker from './services/sneakerData';
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    setData(sneaker);
+  }, []);
+
   return (
-    <Header />
+    <>
+      <Header />
+      <main className={styles.container}>
+        <div className={styles.containerSlider}>
+          <button type="button" className={styles.buttonPrev}>
+            <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+              <path d="M11 1 3 9l8 8" stroke="#1D2026" strokeWidth="3" fill="none" fillRule="evenodd" />
+            </svg>
+          </button>
+
+          <div className={styles.containerImg}>
+            {data?.gallery?.map((item) => (
+              <img key={item.id} src={item.sneakerImg} alt={item.altText} data-index={item.id} />
+            ))}
+          </div>
+
+          <button type="button" className={styles.buttonNext}>
+            <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
+              <path d="m2 1 8 8-8 8" stroke="#1D2026" strokeWidth="3" fill="none" fillRule="evenodd" />
+            </svg>
+          </button>
+        </div>
+        <div />
+      </main>
+    </>
   );
 }
 
