32d05966a7ceab6f83224e0eed5a17200766697d
Juan Antonio <jamll.muro@gmail.com>
2023-06-28 09:11:26 +0200

    Add a new sneakerData.js file where all sneaker information is located

---
+++ src/services/sneakerData.js
@@ -0,0 +1,44 @@
+const sneaker = {
+  name: 'Fall Limited Edition Sneakers',
+  description: 'These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer saler, they&apos;ll withstand everything the weather can offer.',
+  originalPrice: 250,
+  discount: 50,
+  gallery: [
+    {
+      id: 1,
+      sneakerImg: '/images/image-product-1.jpg',
+    },
+    {
+      id: 2,
+      sneakerImg: '/images/image-product-2.jpg',
+    },
+    {
+      id: 3,
+      sneakerImg: '/images/image-product-3.jpg',
+    },
+    {
+      id: 4,
+      sneakerImg: '/images/image-product-4.jpg',
+    },
+  ],
+  thumbnails: [
+    {
+      id: 1,
+      sneakerThumbnail: '/images/image-product-1-thumbnail.jpg',
+    },
+    {
+      id: 2,
+      sneakerThumbnail: '/images/image-product-2-thumbnail.jpg',
+    },
+    {
+      id: 3,
+      sneakerThumbnail: '/images/image-product-3-thumbnail.jpg',
+    },
+    {
+      id: 4,
+      sneakerThumbnail: '/images/image-product-4-thumbnail.jpg',
+    },
+  ],
+};
+
+export default sneaker;
