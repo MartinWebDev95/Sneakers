1128f7b2def00077add412ba3aff3ea759030929
Juan Antonio <jamll.muro@gmail.com>
2023-07-03 12:52:43 +0200

    Change the Cart component to show the products or a message when there are not products

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -2,17 +2,27 @@ import useCartContext from '../../hooks/useCartContext';
 import styles from './Cart.module.css';
 
 function Cart() {
-  const { hiddenCart } = useCartContext();
+  const { hiddenCart, cart } = useCartContext();
 
   return (
     <div className={styles.cart} aria-hidden={hiddenCart}>
       <h3 className={styles.cartTitle}>Cart</h3>
 
-      <div className={styles.containerProduct}>
-        <button type="button" className={styles.checkout}>
-          Checkout
-        </button>
-      </div>
+      {cart.length === 0
+        ? (
+          <p className={styles.noItems}>Your cart is empty</p>
+        ) : (
+          <div className={styles.containerProduct}>
+            <ul>
+              {cart.map((item) => (
+                <li>{item.name}</li>
+              ))}
+            </ul>
+            <button type="button" className={styles.checkout}>
+              Checkout
+            </button>
+          </div>
+        )}
     </div>
   );
 }
